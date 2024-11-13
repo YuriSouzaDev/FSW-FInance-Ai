c3aef30e88c9664c4338dbc1cd4f53295bfa418c
YuriSouzaDev <yurisouza.developer@gmail.com>
2024-11-13 08:10:53 -0300

    fix: ensure stripe webhook route is editing clerk user

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -2,23 +2,24 @@ import { clerkClient } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
 import Stripe from 'stripe';
 
-export const POST = async (req: Request) => {
-  const signature = req.headers.get('stripe-signature');
+export const POST = async (request: Request) => {
+  if (!process.env.STRIPE_SECRET_KEY || !process.env.STRIPE_WEBHOOK_SECRET) {
+    return NextResponse.error();
+  }
+  const signature = request.headers.get('stripe-signature');
   if (!signature) {
     return NextResponse.error();
   }
-  const text = await req.text();
-  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  const text = await request.text();
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
     apiVersion: '2024-10-28.acacia',
   });
-  if (!process.env.STRIPE_WEBHOOK_SECRET) {
-    return NextResponse.error();
-  }
   const event = stripe.webhooks.constructEvent(
     text,
     signature,
     process.env.STRIPE_WEBHOOK_SECRET,
   );
+
   switch (event.type) {
     case 'invoice.paid': {
       // Atualizar o usuário com o seu novo plano
@@ -28,7 +29,7 @@ export const POST = async (req: Request) => {
       if (!clerkUserId) {
         return NextResponse.error();
       }
-      (await clerkClient()).users.updateUser(clerkUserId, {
+      await clerkClient().users.updateUser(clerkUserId, {
         privateMetadata: {
           stripeCustomerId: customer,
           stripeSubscriptionId: subscription,
@@ -44,12 +45,11 @@ export const POST = async (req: Request) => {
       const subscription = await stripe.subscriptions.retrieve(
         event.data.object.id,
       );
-
-      const clerkUserId = subscription.metadata?.clerk_user_id;
+      const clerkUserId = subscription.metadata.clerk_user_id;
       if (!clerkUserId) {
         return NextResponse.error();
       }
-      (await clerkClient()).users.updateUser(clerkUserId, {
+      await clerkClient().users.updateUser(clerkUserId, {
         privateMetadata: {
           stripeCustomerId: null,
           stripeSubscriptionId: null,
@@ -58,7 +58,6 @@ export const POST = async (req: Request) => {
           subscriptionPlan: null,
         },
       });
-      break;
     }
   }
   return NextResponse.json({ received: true });
