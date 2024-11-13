ea5ade1e9caf83a60e0b76a731472309533b9232
YuriSouzaDev <yurisouza.developer@gmail.com>
2024-11-13 20:46:56 -0300

    chore: add tailwind prettier plugin

--- app/(routes)/transactions/page.tsx
+++ app/(routes)/transactions/page.tsx
@@ -1,24 +1,24 @@
-import AddTransactionButton from '@/app/_components/add-transaction-button';
-import { DataTable } from '@/app/_components/ui/data-table';
-import { ScrollArea } from '@/app/_components/ui/scroll-area';
-import { canUserAddTransaction } from '@/app/_data/can-user-add-transaction';
-import { db } from '@/app/_lib/prisma';
-import { auth } from '@clerk/nextjs/server';
-import { redirect } from 'next/navigation';
-import { transationsColumns } from './_columns';
+import AddTransactionButton from "@/app/_components/add-transaction-button";
+import { DataTable } from "@/app/_components/ui/data-table";
+import { ScrollArea } from "@/app/_components/ui/scroll-area";
+import { canUserAddTransaction } from "@/app/_data/can-user-add-transaction";
+import { db } from "@/app/_lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { transationsColumns } from "./_columns";
 
 const TransactionsPage = async () => {
   const { userId } = await auth();
   if (!userId) {
-    redirect('/login');
+    redirect("/login");
   }
   const transactions = await db.transaction.findMany({
     where: { userId },
-    orderBy: { createdAt: 'desc' },
+    orderBy: { createdAt: "desc" },
   });
   const userCanAddTransaction = await canUserAddTransaction();
   return (
-    <div className="p-6 space-y-6 h-full overflow-hidden flex flex-col">
+    <div className="flex h-full flex-col space-y-6 overflow-hidden p-6">
       <div className="flex w-full items-center justify-between">
         <h1 className="text-2xl font-bold">Transações</h1>
         <AddTransactionButton userCanAddTransaction={userCanAddTransaction} />
